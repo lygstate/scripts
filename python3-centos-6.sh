10c7df2923366c9987444c55507a16366547423d
Yonggang Luo <luoyonggang@gmail.com>
2022-05-16 01:41:31 +0800

    building with ssl and ffi, using 3.10 for resolve ssl issue

--- python3-centos-6.sh
+++ python3-centos-6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script setups Python 3.9 alternate installation
+# This script setups Python 3.10 alternate installation
 # on CentOS 6
 # Usage: wget -O python3-centos-6.sh https://raw.githubusercontent.com/vinodpandey/scripts/master/python3-centos-6.sh
 # chmod +x python3-centos-6.sh
@@ -10,7 +10,7 @@
 # License: https://github.com/getredash/redash/blob/master/LICENSE
 
 DIR=/tmp
-VERSION=3.9.12
+VERSION=3.10.4
 URL="https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz"
 TARBALL=/tmp/Python-${VERSION}.tgz
 
@@ -30,31 +30,55 @@ verify_root() {
 }
 
 check_python_version() {
-    if [[ $(/usr/local/bin/python3.9 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))') == 3.9.* ]]; then
-    	SYS_VERSION=$(/usr/local/bin/python3.9 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+    if [[ $(/usr/local/bin/python3.10 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))') == 3.10.* ]]; then
+    	SYS_VERSION=$(/usr/local/bin/python3.10 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
     	echo Python $SYS_VERSION already installed
     	exit 1
     fi
 }
 
 install_system_packages() {
-    yum install -y zlib-devel bzip2-devel
+    yum install -y zlib-devel bzip2-devel libffi-devel
+}
+
+install_openssl() {
+    # https://bugs.python.org/issue43466
+    rm -rf openssl-1.1.1n
+    if [[ ! -f "openssl-1.1.1n.tar.gz" ]]; then
+        wget --no-check-certificate https://www.openssl.org/source/old/1.1.1/openssl-1.1.1n.tar.gz -O openssl-1.1.1n.tar.gz
+    fi
+    tar xf openssl-1.1.1n.tar.gz && \
+    pushd openssl-1.1.1n && \
+        ./config --prefix=/opt/openssl \
+        --openssldir=$(find /etc/ -name openssl.cnf -printf "%h" 2>/dev/null) &&
+    make -j$(nproc) && make install_sw && popd
 }
 
 extract_python_source() {
-    wget "$URL" -O "$TARBALL"
+    if [[ ! -f "$TARBALL" ]]; then
+        wget --no-check-certificate "$URL" -O "$TARBALL"
+    fi
+    rm -rf Python-${VERSION}
     tar -C "$DIR" -xvf "$TARBALL"
 }
  
 install_python() {
     cd Python-${VERSION}
-    ./configure --enable-optimizations
-    make altinstall
+    ./configure --enable-optimizations \
+        --with-openssl=/opt/openssl \
+        --prefix=/usr \
+        --with-openssl-rpath=auto
+    make -j$(nproc) altinstall
+    # https://stackoverflow.com/questions/51201459/python-3-7-install-not-working-on-opensuse-leap-42-3?rq=1
+    rm -f /usr/lib/python3.10/lib-dynload
+    ln -s /usr/lib64/python3.10/lib-dynload/ /usr/lib/python3.10/lib-dynload
+    python3.10 -m pip install --upgrade pip setuptools
 }
 
 verify_root
 check_python_version
 install_system_packages
+install_openssl
 extract_python_source
 install_python
 
