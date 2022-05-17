7b68991fbc501c7aca44f3587692b9821bb143f5
lygstate <lygstate@localhost.localdomain>
2022-05-17 20:46:54 +0800

    Try building wayland properly on

--- meson-centos-6.sh
+++ meson-centos-6.sh
@@ -5,12 +5,23 @@ ln -s /usr/bin/python3.10 /usr/bin/python3
 # For libdrm
 yum install -y libpciaccess-devel
 
-# For mesa
-yum install -y expat-devel
-
 
 # Build drm
-meson --prefix=/usr
+CC=clang CXX=clang++ meson --prefix=/usr
 
+# For wayland
+yum install -y libxml2-devel libdot-devel
+
+# Build wayland
+git reset --hard 1.20.0
+CC=clang CXX=clang++ meson --prefix=/usr -Ddocumentation=false
+
+# Build wayland-protocols
+git reset --hard 1.25
+CC=clang CXX=clang++ meson --prefix=/usr
+
+# For mesa
+yum install -y expat-devel flex bison
+yum install -y libX11-devel --setopt=protected_multilib=false
 # Build mesa
-CC=clang CXX=clang++ meson
+CC=clang CXX=clang++ meson --prefix=/usr -D llvm=enabled -Dshared-llvm=disabled
