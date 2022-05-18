2f31acf981a6f3cebc11355ccd5a42758624fc63
lygstate <lygstate@localhost.localdomain>
2022-05-18 11:37:57 +0800

    try to fixes install upgrade

--- meson-centos-6.sh
+++ meson-centos-6.sh
@@ -5,9 +5,12 @@ ln -s /usr/bin/python3.10 /usr/bin/python3
 
 # Reset yum cache
 sudo ls
+
+# Wait password
+
 sudo rm -rf /var/cache/yum/*
 sudo yum clean all
-sudo yum update
+sudo yum update 1>out.txt 2>err.txt
 
 # Install clang
 sudo tar xf llvm-install.tar.xz -C /usr
@@ -21,13 +24,15 @@ yum install -y libpciaccess-devel
 
 # Build drm
 git clone https://gitlab.freedesktop.org/mesa/drm.git drm
+git reset --hard libdrm-2.4.109
 mkdir drm/build
 pushd drm/build
 CC=clang CXX=clang++ meson --prefix=/usr
-meson --prefix=/usr
+ninja
+sudo ninja install
 
 # For wayland
-yum install -y libxml2-devel libffi-devel expat-devel
+sudo yum install -y libxml2-devel libffi-devel expat-devel
 
 # Build wayland
 git clone https://gitlab.freedesktop.org/wayland/wayland wayland
@@ -35,13 +40,14 @@ mkdir wayland/build
 pushd wayland/build
 git reset --hard 1.20.0
 CC=clang CXX=clang++ meson --prefix=/usr -Ddocumentation=false
+ninja
 sudo ninja install
 
 # Build wayland-protocols
 git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git wayland-protocols
-git reset --hard 1.25
 mkdir wayland-protocols/build
 pushd wayland-protocols/build
+git reset --hard 1.25
 CC=clang CXX=clang++ meson --prefix=/usr
 ninja
 sudo ninja install
@@ -50,20 +56,36 @@ sudo ninja install
 git clone https://github.com/KhronosGroup/glslang.git glslang
 mkdir glslang/build
 pushd glslang/build
+git reset --hard sdk-1.3.211.0
 CC=clang CXX=clang++  cmake .. -DCMAKE_INSTALL_PREFIX=/usr -GNinja
 ninja
 sudo ninja install
 
 # For mesa
-sudo yum install -y flex bison pixman-devel libXrandr-devel xorg-x11-server-devel xorg-x11-server-Xorg
-sudo yum install -y libxshmfence-devel
+sudo yum install -y flex bison pixman-devel xorg-x11-server-devel xorg-x11-server-Xorg libxshmfence-devel libXrandr-devel
+sudo yum install -y libva-devel libvdpau-devel
 # Build mesa
 git clone https://gitlab.freedesktop.org/mesa/mesa.git mesa
-CC=clang CXX=clang++ meson --prefix=/usr -D llvm=enabled -Dshared-llvm=disabled
+mkdir mesa/build
+pushd mesa/build
+git reset --hard mesa-22.0.3
+CC=clang CXX=clang++ meson --prefix=/usr \
+-D llvm=enabled \
+-Dshared-llvm=disabled \
+-D glx=dri \
+-D gbm=enabled \
+-D egl=enabled \
+-D platforms=x11,wayland \
+-D dri3=enabled \
+-D gallium-extra-hud=true \
+-D gallium-vdpau=enabled \
+-D gallium-va=enabled \
+-D gallium-xa=enabled
 
 # xf86-video-amdgpu
 sudo yum install -y xorg-x11-util-macros
  ./autogen.sh --prefix=/usr
- make
- sudo make install
- 
+make
+sudo make install
+
+export DISPLAY=":0.0"
