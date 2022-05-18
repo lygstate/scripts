6eccccb61a8700ee45772b2d019dec1cecd4cf5f
lygstate <lygstate@localhost.localdomain>
2022-05-18 21:53:37 +0800

    Built as much as possible

--- meson-centos-6.sh
+++ meson-centos-6.sh
@@ -26,6 +26,7 @@ sudo yum install -y libpciaccess-devel
 
 # Build drm
 git clone https://gitlab.freedesktop.org/mesa/drm.git drm
+rm -rf drm/build 
 mkdir drm/build
 pushd drm/build
 git reset --hard libdrm-2.4.109
@@ -37,8 +38,11 @@ popd
 # For wayland
 sudo yum install -y libxml2-devel xz-devel libffi-devel expat-devel
 
+# 
+
 # Build wayland
 git clone https://gitlab.freedesktop.org/wayland/wayland wayland
+rm -rf wayland/build 
 mkdir wayland/build
 pushd wayland/build
 git reset --hard 1.20.0
@@ -49,6 +53,7 @@ popd
 
 # Build wayland-protocols
 git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git wayland-protocols
+rm -rf wayland-protocols/build 
 mkdir wayland-protocols/build
 pushd wayland-protocols/build
 git reset --hard 1.25
@@ -59,6 +64,7 @@ popd
 
 # mesa deps
 git clone https://github.com/KhronosGroup/glslang.git glslang
+rm -rf glslang/build
 mkdir glslang/build
 pushd glslang/build
 git reset --hard sdk-1.3.211.0
@@ -67,14 +73,28 @@ ninja
 sudo ninja install
 popd
 
+# libva-devel need build from source, it's too old
+# Dependency libva found: NO found 0.38.0 but need: '>= 1.8.0'
+
+git clone https://github.com/intel/libva.git libva
+rm -rf libva/build
+mkdir -p libva/build
+pushd libva/build
+git reset --hard 2.14.0
+CC=clang CXX=clang++ meson --prefix=/usr
+ninja
+sudo ninja install
+popd
+
 # For mesa
 sudo yum install -y flex bison \
     pixman-devel \
     xorg-x11-server-devel xorg-x11-server-Xorg libxshmfence-devel libXrandr-devel \
-    libva-devel libvdpau-devel
+    libvdpau-devel libXvMC-devel libXv-devel libomxil-bellagio-devel
+
 # Build mesa
 git clone https://gitlab.freedesktop.org/mesa/mesa.git mesa
-mkdir mesa/build
+mkdir -p mesa/build
 pushd mesa/build
 git reset --hard mesa-22.0.3
 CC=clang CXX=clang++ meson --prefix=/usr \
@@ -87,8 +107,11 @@ CC=clang CXX=clang++ meson --prefix=/usr \
 -D dri3=enabled \
 -D gallium-extra-hud=true \
 -D gallium-vdpau=enabled \
--D gallium-va=disabled \
--D gallium-xa=enabled
+-D gallium-va=enabled \
+-D gallium-xa=enabled \
+-D gallium-omx=bellagio \
+-D osmesa=true
+popd
 
 # xf86-video-amdgpu
 sudo yum install -y xorg-x11-util-macros
