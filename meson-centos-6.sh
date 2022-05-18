b0486739561f08d78f54a486892066175aec312e
lygstate <lygstate@localhost.localdomain>
2022-05-18 20:53:14 +0800

    Install all dependencies properly

--- meson-centos-6.sh
+++ meson-centos-6.sh
@@ -13,27 +13,29 @@ sudo yum clean all
 sudo yum update 1>out.txt 2>err.txt
 
 # Install clang
+cd ~/work
 sudo tar xf llvm-install.tar.xz -C /usr
 sudo cp -rf /usr/lib/x86_64-unknown-linux-gnu/* /usr/lib64/
+ls /usr/lib64/libc++* 
 
 # For git clone
 sudo yum install -y ca-certificates
 
 # For libdrm
-yum install -y libpciaccess-devel
+sudo yum install -y libpciaccess-devel
 
 # Build drm
 git clone https://gitlab.freedesktop.org/mesa/drm.git drm
-git reset --hard libdrm-2.4.109
 mkdir drm/build
 pushd drm/build
+git reset --hard libdrm-2.4.109
 CC=clang CXX=clang++ meson --prefix=/usr
 ninja
 sudo ninja install
 popd
 
 # For wayland
-sudo yum install -y libxml2-devel libffi-devel expat-devel
+sudo yum install -y libxml2-devel xz-devel libffi-devel expat-devel
 
 # Build wayland
 git clone https://gitlab.freedesktop.org/wayland/wayland wayland
@@ -43,6 +45,7 @@ git reset --hard 1.20.0
 CC=clang CXX=clang++ meson --prefix=/usr -Ddocumentation=false
 ninja
 sudo ninja install
+popd
 
 # Build wayland-protocols
 git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git wayland-protocols
@@ -52,20 +55,23 @@ git reset --hard 1.25
 CC=clang CXX=clang++ meson --prefix=/usr
 ninja
 sudo ninja install
+popd
 
 # mesa deps
 git clone https://github.com/KhronosGroup/glslang.git glslang
 mkdir glslang/build
 pushd glslang/build
 git reset --hard sdk-1.3.211.0
-CC=clang CXX=clang++  cmake .. -DCMAKE_INSTALL_PREFIX=/usr -GNinja
+CC=clang CXX=clang++ cmake .. -DCMAKE_INSTALL_PREFIX=/usr -GNinja
 ninja
 sudo ninja install
 popd
 
 # For mesa
-sudo yum install -y flex bison pixman-devel xorg-x11-server-devel xorg-x11-server-Xorg libxshmfence-devel libXrandr-devel
-sudo yum install -y libva-devel libvdpau-devel
+sudo yum install -y flex bison \
+    pixman-devel \
+    xorg-x11-server-devel xorg-x11-server-Xorg libxshmfence-devel libXrandr-devel \
+    libva-devel libvdpau-devel
 # Build mesa
 git clone https://gitlab.freedesktop.org/mesa/mesa.git mesa
 mkdir mesa/build
