bcc67e6f668126f195639b384c92fe71471faf64
lygstate <lygstate@localhost.localdomain>
2022-05-23 03:25:09 +0800

    Now opencl also built

--- meson-centos-6.sh
+++ meson-centos-6.sh
@@ -19,7 +19,7 @@ sudo yum update 1>out.txt 2>err.txt
 cd ~/work
 sudo tar xf llvm-install.tar.xz -C /usr
 sudo cp -rf /usr/lib/x86_64-unknown-linux-gnu/* /usr/lib64/
-ls /usr/lib64/libc++* 
+ls -la /usr/lib64/libc++* 
 
 # For git clone
 sudo yum install -y ca-certificates
@@ -41,8 +41,6 @@ popd
 
 # For wayland
 sudo yum install -y libxml2-devel xz-devel libffi-devel expat-devel
-
-# 
 
 # Build wayland
 git clone https://gitlab.freedesktop.org/wayland/wayland wayland
@@ -66,13 +64,37 @@ ninja
 sudo ninja install
 popd
 
+# For spirv tools
+if [[ ! -f "SPIRV-Headers-sdk-1.3.211.0.tar.gz" ]]; then
+    curl -LJO https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/sdk-1.3.211.0.tar.gz
+fi
+rm -rf SPIRV-Headers-sdk-1.3.211.0
+tar xf SPIRV-Headers-sdk-1.3.211.0.tar.gz
+
+# spirv tools
+if [[ ! -f "SPIRV-Tools-2022.2.tar.gz" ]]; then
+    curl -LJO https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/tags/v2022.2.tar.gz
+fi
+rm -rf SPIRV-Tools-2022.2
+tar xf SPIRV-Tools-2022.2.tar.gz
+mkdir SPIRV-Tools-2022.2/build
+pushd SPIRV-Tools-2022.2/build
+CC=clang CXX=clang++ cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja \
+    -DSPIRV-Headers_SOURCE_DIR=`pwd`/../../SPIRV-Headers-sdk-1.3.211.0
+
+ninja
+sudo ninja install
+popd
+
 # mesa deps
 git clone https://github.com/KhronosGroup/glslang.git glslang
 rm -rf glslang/build
 mkdir glslang/build
 pushd glslang/build
 git reset --hard sdk-1.3.211.0
-CC=clang CXX=clang++ cmake .. -DCMAKE_INSTALL_PREFIX=/usr -GNinja
+CC=clang CXX=clang++ cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja
 ninja
 sudo ninja install
 popd
@@ -90,12 +112,42 @@ ninja
 sudo ninja install
 popd
 
+if [[ ! -f "SPIRV-LLVM-Translator-14.0.0.tar.gz" ]]; then
+    curl -LJO https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v14.0.0.tar.gz
+fi
+rm -rf SPIRV-LLVM-Translator-14.0.0
+tar xf SPIRV-LLVM-Translator-14.0.0.tar.gz
+mkdir -p SPIRV-LLVM-Translator-14.0.0/build
+pushd SPIRV-LLVM-Translator-14.0.0/build
+CC=clang CXX=clang++ cmake .. \
+-DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja \
+-DLLVM_LIBDIR_SUFFIX=64 \
+-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=`pwd`/../../SPIRV-Headers-sdk-1.3.211.0
+
+ninja llvm-spirv
+sudo ninja install
+sudo cp tools/llvm-spirv/llvm-spirv /usr/bin/
+popd
+
+if [[ ! -f "libclc-14.0.3.src.tar.xz" ]]; then
+    curl -LJO https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.3/libclc-14.0.3.src.tar.xz
+fi
+rm -rf libclc-14.0.3.src
+tar xf libclc-14.0.3.src.tar.xz
+mkdir -p libclc-14.0.3.src/build
+pushd libclc-14.0.3.src/build
+CC=clang CXX=clang++ cmake .. \
+-DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja
+
+ninja
+sudo ninja install
+popd
+
 # For mesa
 sudo yum install -y flex bison \
     pixman-devel \
     xorg-x11-server-devel xorg-x11-server-Xorg libxshmfence-devel libXrandr-devel \
     libvdpau-devel libXvMC-devel libXv-devel libomxil-bellagio-devel elfutils-libelf-devel
-
 
 # Build mesa
 git clone https://gitlab.freedesktop.org/lygstate/mesa.git mesa
@@ -112,9 +164,13 @@ CC=clang CXX=clang++ meson --prefix=/usr \
 -D dri3=enabled \
 -D gallium-extra-hud=true \
 -D gallium-vdpau=enabled \
+-D gallium-xvmc=enabled \
+-D gallium-omx=bellagio \
 -D gallium-va=enabled \
 -D gallium-xa=enabled \
--D gallium-omx=bellagio \
+-D gallium-nine=true \
+-D gallium-opencl=icd \
+-D opencl-spirv=true \
 -D osmesa=true \
 -D buildtype=release \
 -D b_lto=true \
