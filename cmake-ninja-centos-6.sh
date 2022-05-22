0061931e8995ea1727c2868aa25a0965dcd79bb7
lygstate <lygstate@localhost.localdomain>
2022-05-22 15:58:40 +0800

    Use curl instead wget

--- cmake-ninja-centos-6.sh
+++ cmake-ninja-centos-6.sh
@@ -6,16 +6,16 @@ pushd /tmp
 yum install -y gcc gcc-c++
 
 if [[ ! -f "cmake-3.23.1-linux-x86_64.sh" ]]; then
-    wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-linux-x86_64.sh -O cmake-3.23.1-linux-x86_64.sh
+    curl -LJO https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-linux-x86_64.sh
 fi
 chmod +x cmake-3.23.1-linux-x86_64.sh
 ./cmake-3.23.1-linux-x86_64.sh --skip-license --prefix=/usr
 
-if [[ ! -f "ninja.tar.gz" ]]; then
-    wget --no-check-certificate https://github.com/ninja-build/ninja/archive/refs/tags/v1.11.0.tar.gz -O ninja.tar.gz
+if [[ ! -f "ninja-1.11.0.tar.gz" ]]; then
+    curl -LJO https://github.com/ninja-build/ninja/archive/refs/tags/v1.11.0.tar.gz
 fi
 rm -rf ninja-1.11.0
-tar xf ninja.tar.gz
+tar xf ninja-1.11.0.tar.gz
 mkdir ninja-1.11.0/build
 pushd ninja-1.11.0/build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr
--- python3-centos-6.sh
+++ python3-centos-6.sh
@@ -45,7 +45,7 @@ install_openssl() {
     # https://bugs.python.org/issue43466
     rm -rf openssl-1.1.1n
     if [[ ! -f "openssl-1.1.1n.tar.gz" ]]; then
-        wget --no-check-certificate https://www.openssl.org/source/old/1.1.1/openssl-1.1.1n.tar.gz -O openssl-1.1.1n.tar.gz
+        curl -LJO https://www.openssl.org/source/old/1.1.1/openssl-1.1.1n.tar.gz
     fi
     tar xf openssl-1.1.1n.tar.gz && \
     pushd openssl-1.1.1n && \
@@ -56,7 +56,7 @@ install_openssl() {
 
 extract_python_source() {
     if [[ ! -f "$TARBALL" ]]; then
-        wget --no-check-certificate "$URL" -O "$TARBALL"
+        curl -LJO "$URL"
     fi
     rm -rf Python-${VERSION}
     tar -C "$DIR" -xvf "$TARBALL"
